580252864eeebe6c1e14d8b28a51856db43e8a6f
su <su@DESKTOP-93TC6F5>
2021-10-20 12:30:19 +0200

    Добавьте файлы проекта.

---
+++ ConsoleApp16/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace ConsoleApp16
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("Введите x:");
+                double x = double.Parse(Console.ReadLine());
+                double y = 0;
+                if (x != 0) y = 1 / (x * x);
+                Console.WriteLine($"x={x} y={y:F2}");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
